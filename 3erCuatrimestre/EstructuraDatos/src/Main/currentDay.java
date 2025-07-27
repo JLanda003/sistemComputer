c45292e41cc12fd2a5eaaccd5c811a382026d532
Jlandav <98247950+JLanda003@users.noreply.github.com>
2025-07-27 07:43:36 -0500

    //Add
    Ejercicio 26

---
+++ 3erCuatrimestre/EstructuraDatos/src/Main/currentDay.java
@@ -0,0 +1,49 @@
+/*
+* Ingresar numeros del 1 - 7
+* Mostrar día de la semana
+*/
+
+import java.util.Scanner;
+
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		int current;
+		String strPrint;
+		
+		System.out.println("Ingrese un número del 1 al 7");
+		current = scan.nextInt();
+		
+		switch(current){
+	      case 1:
+            strPrint = "El día es: Domingo";
+            break;
+          case 2:
+            strPrint = "El día es: Lunes";
+            break;
+          case 3:
+            strPrint = "El día es: Martes";
+            break;
+          case 4:
+            strPrint = "El día es: Miercoles";
+            break;
+          case 5:
+            strPrint = "El día es: Jueves";
+            break;
+          case 6:
+            strPrint = "El día es: Viernes";
+            break;
+          case 7:
+            strPrint = "El día es: Sabado";
+            break;
+          default:
+            strPrint = "Coleque un numero valido del 1 al 7";
+		}
+		
+		System.out.println(strPrint);
+		
+		scan.close();
+	}
+}
