cbacdc0c23397e24c920dcbf246adf46359602cc
Jlandav <98247950+JLanda003@users.noreply.github.com>
2025-07-27 08:25:46 -0500

    Add files via upload

---
+++ 3erCuatrimestre/EstructuraDatos/src/Main/example1.java
@@ -0,0 +1,15 @@
+// El numero es mayor a 5
+public class main
+{
+    public static void main(String[] args)
+    {
+        int numero = 10;
+
+        if (numero > 5){
+            System.out.println("Mayor a 5");
+        }else{
+            System.out.println("Menor o igual a 5");
+        }
+    }
+}
+//Joshua Landa Vivas
---
+++ 3erCuatrimestre/EstructuraDatos/src/Main/example2.java
@@ -0,0 +1,17 @@
+// El numero es mayor a 5, pero menor o igual que 10
+public class main
+{
+    public static void main(String[] args)
+    {
+        int numero = 11;
+
+        if (numero > 10){
+            System.out.println("Mayor que 10");
+        } else if (numero > 5){
+            System.out.println("Mayor a 5 pero menor o igual que 10");
+        } else {
+            System.out.println("Menor o igual a 5");
+        }
+    }
+}
+//Joshua Landa Vivas
