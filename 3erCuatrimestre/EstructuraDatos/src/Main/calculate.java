f260bc3e84b3c2e2eebad8c75100dd1739827871
Jlandav <98247950+JLanda003@users.noreply.github.com>
2025-07-27 08:32:38 -0500

    //Add
    Calcular un valor de dos numeros con una clave

---
+++ 3erCuatrimestre/EstructuraDatos/src/Main/calculate.java
@@ -0,0 +1,47 @@
+/*
+* Ingresar dos numeros
+* Ingresa una clave + - * /
+* Motrar el resultado
+*/
+
+import java.util.Scanner;
+
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		double currentA, currentB;
+		String strPrint, key;
+		
+		System.out.println("Ingrese la clave (+  -  *  /)");
+		key = scan.nextLine();
+		
+		System.out.println("Ingrese un el primer numero");
+		currentA = scan.nextInt();
+		
+		System.out.println("Ingrese un el segundo numero");
+		currentB = scan.nextInt();
+
+		switch(key){
+	      case "+":
+            strPrint = Double.toString(currentA + currentB);
+            break;
+	      case "-":
+            strPrint = Double.toString(currentA + currentB);
+            break;
+          case "*":
+            strPrint = Double.toString(currentA + currentB);
+            break;
+          case "/":
+            strPrint = Double.toString(currentA + currentB);
+            break;
+          default:
+            strPrint = "Coleque una clave correcta";
+		}
+		
+		System.out.println("El resultado es: " + strPrint);
+		
+		scan.close();
+	}
+}
