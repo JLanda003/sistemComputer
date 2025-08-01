9a9e6cd2d8e5c501954da748a9e88e77a0a36597
Jlandav <98247950+JLanda003@users.noreply.github.com>
2025-07-31 22:37:22 -0500

    //Add
    Ejercicio 23, Programa que permite introducir un numero entre 1 y 12 y muestre en pantalla el numero de días de ese mes, así como su nombre.

---
+++ 3erCuatrimestre/DataStructure/src/Main/MonthDays.java
@@ -0,0 +1,63 @@
+package Main;
+
+import java.util.Scanner;
+
+/**
+ * Permite ingresar numeros del 1 al 12
+ * Mostrara en pantalla
+ * Días del mes
+ * Nombre del mes
+ */
+
+public class MonthDays {
+
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+
+    int month;
+    String daysMonth = "", messageString = "";
+
+    System.out.println("Ingrese un numero del 1 al 12");
+
+    month = scan.nextInt();
+
+    /*Inicializamos la varible para mostrar el mes en texto*/
+    String[] months = {
+      "Enero", "Febrero",
+      "Marzo", "Abril",
+      "Mayo", "Junio",
+      "Julio", "Agosto",
+      "Septiembre","Octubre",
+      "Noviembre","Diciembre"
+    };
+
+    switch (month) {
+      case 1, 3, 5, 7, 8, 10, 12:
+        daysMonth = "31";
+        break;
+      case 2:
+        daysMonth = "28";
+        break;
+      case 4, 6, 9, 11:
+        daysMonth = "30";
+        break;
+      default:
+        messageString = "Ingrese un numero del 1 al 12";
+        System.out.println(messageString);
+        break;
+    }
+
+    if (messageString != ""){
+      scan.close();
+      return;
+    }
+
+    messageString = "El numero: " + month
+      + " representa al mes: " + months[month - 1]
+      + " y cuenta con: " + daysMonth + " dias";
+
+    System.out.println(messageString);
+
+    scan.close();
+  }
+}
